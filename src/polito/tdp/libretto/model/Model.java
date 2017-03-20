1e4e38f728e1aa53d4b38e3fb27ead02a8a287be
Rebecca <Rebecca@172.20.236.35>
2017-03-20 10:41:56 +0100

    class model

--- src/polito/tdp/libretto/model/Model.java
+++ src/polito/tdp/libretto/model/Model.java
@@ -1,5 +1,41 @@
 package polito.tdp.libretto.model;
 
+import java.util.LinkedList;
+import java.util.List;
+
 public class Model {
+	
+	private List <Esame> esami;
+	
+	public Model(){
+		this.esami= new LinkedList <Esame>();
+	}
+	/**
+	 * aggiunge un esame all'elenco degli esami presenti
+	 * verificando che non ci sia già
+	 * @param e
+	 */
+	public boolean addEsame(Esame e){
+		if (!esami.contains(e)){  //funzione perchè ho implementato equals in esame!!
+			esami.add(e);
+			return true;          //se l'operazione è andata buon fine
+		}else
+			return false;		 // se l'operazione non è andata a buon fine
+	 
+	}
+	/**
+	 * ricerca se esiste un esame col codice specificato
+	 * se esiste restituisce l'esame
+	 * se non esiste restituisce nulll
+	 * @param codice esame da ricercare
+	 * @return l'esame trovato oppure null
+	 */
+	public Esame trovaEsame (String codice){
+		int pos = esami.indexOf(new Esame (codice, null, null));
+		if (pos==-1)
+			return null;
+		else
+			return esami.get(pos);
+	}
 
 }
